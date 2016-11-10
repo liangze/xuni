a0c29412f007b74e141e9cc7962f355d137d61c2
LC <1396511041@qq.com>
2016-11-10 17:30:30 +0800

    11

--- Web/admin/zhuye.aspx.cs
+++ Web/admin/zhuye.aspx.cs
@@ -23,21 +23,6 @@ namespace Web.admin
     public partial class zhuye : AdminPageBase//System.Web.UI.Page
     {
         public string AllIn = string.Empty;
-        lgk.BLL.tb_user u = new lgk.BLL.tb_user();
-        public int shuliang { get; set; }
-        public int leiji { get; set; }
-        public string jifen { get; set; }
-        public string jifenleiji { get; set; }
-        public string Allyu = string.Empty;
-        public string jiangjin  { get; set; }
-        public string jiangjinleiji  { get; set; }
-        public string aixing { get; set; }
-        public string zongyeji { get; set; }
-        public string fahuo { get; set; }
-        public string goumai { get; set; }
-        public string youjian { get; set; }
-        public string tixian { get; set; }
-        public string tixianchuli { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -143,73 +128,7 @@ namespace Web.admin
             decimal zsy = cashsellBLL.GetAlready(strWhere);
             decimal zsy1 = cashsellBLL.GetAlready(strWhere1);
             AllIn = zsy.ToString();
-            Allyu = zsy1.ToString();
 
         }
-
-        protected void Total() // 
-        {
-
-            DateTime time = DateTime.Now.Date;
-            string sql = "select *from  tb_user where RegTime>= '" + time + "';select * from tb_user ;select sum(User011)a from tb_user where userid<>1 ; select sum(LeftScore +RightScore)a from tb_user where userid=1 ";
-            DataSet ds = u.getData_Chaxun(sql, "");
-            DataTable dt = ds.Tables[0];
-            DataTable dt1 = ds.Tables[1];
-            DataTable dt2 = ds.Tables[2];
-            DataTable dt3 = ds.Tables[3];
-            zongyeji= dt3.Rows[0]["a"].ToString();
-            aixing = dt2.Rows[0]["a"].ToString();
-            shuliang = dt.Rows.Count;
-            leiji = dt1.Rows.Count;
-        }
-
-        protected void jifen1() // 
-        {
-            DateTime time = DateTime.Now.Date;
-            string sql = "select sum(Number)a from  Cashbuy where BuyDate>= '" + time + "' and UserID<>1  ;select sum(Number)a from Cashbuy  ";
-            DataSet ds = u.getData_Chaxun(sql, "");
-            DataTable dt = ds.Tables[0];
-            DataTable dt1 = ds.Tables[1];
-            jifen = dt.Rows[0]["a"].ToString();
-            jifenleiji = dt1.Rows[0]["a"].ToString();
-
-
-        }
-
-        protected void jiangjin1() // 
-        {
-            DateTime time = DateTime.Now.Date;
-            string sql = "select sum(sf)a from  tb_bonus where AddTime>= '" + time + "'and UserID<>1  ;select sum(sf)a from tb_bonus  ";
-            DataSet ds = u.getData_Chaxun(sql, "");
-            DataTable dt = ds.Tables[0];
-            DataTable dt1 = ds.Tables[1];
-            jiangjin = dt.Rows[0]["a"].ToString();
-            jiangjinleiji = dt1.Rows[0]["a"].ToString();
-
-
-        }
-        protected void gouwu() // 
-        {
-            string sql = "select count(*)a from  tb_Order  ;select count(*)a from tb_Order where IsSend=2 ";
-            DataSet ds = u.getData_Chaxun(sql, "");
-            DataTable dt = ds.Tables[0];
-            DataTable dt1 = ds.Tables[1];
-            goumai = dt.Rows[0]["a"].ToString();
-            fahuo = dt1.Rows[0]["a"].ToString();
-        }
-        protected void tixian1() // 
-        {
-            DateTime time = DateTime.Now.Date;
-            string sql = "select count(*)a from  tb_takeMoney  ;select count(*)a from tb_takeMoney where Flag=1; select count(*)a from [tb_leaveMsg] where LeaveTime>='" + time + "' ";
-            DataSet ds = u.getData_Chaxun(sql, "");
-            DataTable dt = ds.Tables[0];
-            DataTable dt1 = ds.Tables[1];
-            DataTable dt2 = ds.Tables[2];
-            youjian= dt2.Rows[0]["a"].ToString();
-            tixian = dt.Rows[0]["a"].ToString();
-            tixianchuli = dt1.Rows[0]["a"].ToString();
-        }
-
-
-        }
+    }
 }
